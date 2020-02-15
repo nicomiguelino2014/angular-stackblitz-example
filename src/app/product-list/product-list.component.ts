e374851cd53bade0754440acb9ff23dbad978e4e
Miguelino <nico.miguelino@nokia.com>
2020-02-15 17:09:18 +0800

    Modified product-list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,8 @@ import { products } from '../products';
 export class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  share(product) {
+    window.alert(`The product ${product.name} has been shared!`);
   }
 }
 
